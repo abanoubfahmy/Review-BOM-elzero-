f99d8cf985004d9072e202bf6d4bdc78f9051da5
abanoubfahmy <168681073+abanoubfahmy@users.noreply.github.com>
2024-10-22 18:01:56 -0700

    Add files via upload

---
+++ Task Bom/part3.js
@@ -0,0 +1,31 @@
+//   part 4
+// let div = document.querySelector("div");
+// let count = window.setInterval(countdown, 1000);
+// function countdown() {
+//     div.innerHTML=parseInt(div.innerHTML)-1;
+//  if (div.innerHTML === "0") {
+//  window.clearInterval(count);
+//  }
+// }
+// countdown()
+// part 5
+// let div = document.querySelector("div");
+// let count = window.setInterval(countdown, 1000);
+
+// function countdown() {
+//     div.innerHTML = parseInt(div.innerHTML) - 1;
+
+//     if (parseInt(div.innerHTML) === 5) {
+//         window.open(
+//             'https://elzero.org/javascript-bootcamp-assignments-lesson-from-102-to-110/',
+//             "_blank",
+//             "width=550px,height=500px,top=50,left=250, bottom=100"
+//         );
+//     }
+
+//     if (parseInt(div.innerHTML) === 0) {
+//         window.clearInterval(count);
+//     }
+// }
+
+// countdown();
---
+++ Task Bom/patrt2.js
@@ -0,0 +1,30 @@
+// part 2
+// function popup(){
+//     let myPopup= document.createElement("div");
+//     myPopup.className="popup";
+//     myPopup.style.cssText="position: absolute; left: 50%; top: 50%; transform: translate(-50%, -50%); background-color: #eee; padding: 20px; border-radius: 6px; text-align: center;"
+
+// let span= document.createElement("span");
+//   span.innerHTML="X"
+//   span.style.cssText="position: absolute; right: -10px; top: -10px; background: red; color: white; font-size: 16px; width: 30px; height: 30px; border-radius: 50%; display: flex; align-items: center; justify-content: center; cursor: pointer;"
+
+// let h2=document.createElement("h2");
+// h2.innerHTML="Welcome";
+// //h2.style.cssText="position: absolute; right: -10px; top: -10px; background: red; color: white; font-size: 16px; width: 30px; height: 30px; border-radius: 50%; display: flex; align-items: center; justify-content: center; cursor: pointer;"
+// let p = document.createElement("p");
+// p.textContent = "Welcome To Elzero Web School";
+
+// span.onclick=function(){
+//   span.parentElement.remove();
+// }
+//   setTimeout(()=>{
+//   myPopup.append(span);
+// myPopup.append(h2)
+// myPopup.append(p);
+// document.body.prepend(myPopup);
+// },5000);
+// }
+
+
+
+// popup();
